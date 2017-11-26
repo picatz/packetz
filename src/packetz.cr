5453cd86e50e5f0dcdba78bac94c2ef1d8fa5910
Kent Gruber <kgruber1@emich.edu>
2017-11-26 10:12:23 -0500

    Allow arguments to be passed and set for yielding capture.

--- src/packetz.cr
+++ src/packetz.cr
@@ -8,4 +8,12 @@ module Packetz
   def self.capture(interface = Packetz.interfaces.default, snaplen = 65535, promisc = 0, timeout_ms = 1)
     Capture.new(interface, snaplen, promisc, timeout_ms)
   end
+  
+  def self.capture(interface = Packetz.interfaces.default, snaplen = 65535, promisc = 0, timeout_ms = 1)
+    cap = Packetz.capture(interface, snaplen, promisc, timeout_ms)
+    cap.start!
+    cap.each do |packet|
+      yield packet
+    end
+  end
 end
