dd3201e44e140a57da77a8c2772e8678c622b193
Kent Gruber <kgruber1@emich.edu>
2017-12-22 17:31:31 -0500

    Update example

--- examples/ethernet_capture_parser.cr
+++ examples/ethernet_capture_parser.cr
@@ -1,6 +1,10 @@
 require "../src/packetz"
 
+counter = 0
 Packetz.capture do |packet|
   packet = Packetz::Parsers::Ethernet.new(packet) 
   puts "#{packet.source} -> #{packet.destination} [#{packet.type}]"
+  sleep 1
+  counter += 1
+  break if counter == 10
 end
